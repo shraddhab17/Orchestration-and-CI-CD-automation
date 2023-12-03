b4d6b2b975573ab79525aee659fa3fc1a5bdea4e
root <root@master-node.Excitel>
2023-12-03 22:40:09 +0530

    first update

---
+++ kubeflow_pipeline.py
@@ -0,0 +1,38 @@
+import kfp
+from kfp import dsl
+
+
+def fetch_code_op():
+    return dsl.ContainerOp(
+        name="Fetch Code",
+        image="alpine/git:latest",
+        command=[
+            "git",
+            "clone",
+            "https://github.com/your-username/your-repo.git",
+            "/src",
+        ],
+    )
+
+
+def deploy_op():
+    return dsl.ContainerOp(
+        name="Deploy",
+        image="bitnami/kubectl:latest",
+        command=["kubectl", "apply", "-f", "/src/kubernetes-manifests/"],
+        pvolumes={"/src": fetch_code_op().pvolume},
+    )
+
+
+@dsl.pipeline(
+    name="My pipeline",
+    description="A pipeline that fetches code from GitHub and deploys it on EKS.",
+)
+def my_pipeline():
+    fetch_code = fetch_code_op()
+    deploy = deploy_op()
+    deploy.after(fetch_code)
+
+
+if __name__ == "__main__":
+    kfp.compiler.Compiler().compile(my_pipeline, "kubeflow-pipeline.zip")
