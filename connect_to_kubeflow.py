1c5fea898d8ee7ef2af61c2878a81a782534a3e0
shraddha <shraddhabhosle96@gmail.com>
2023-12-03 22:57:46 +0530

    added connect to kubeflow.py

---
+++ connect_to_kubeflow.py
@@ -0,0 +1,14 @@
+from kfp import Client
+
+# Define the host URL of your Kubeflow Pipelines deployment
+host_url = 'http://<your-kubeflow-pipelines-host-url>'
+
+# Create a client that connects to the Kubeflow Pipelines API
+client = Client(host=host_url)
+
+# Run your pipeline
+client.run_pipeline(
+    experiment_id='<your-experiment-id>', 
+    job_name='<your-job-name>', 
+    pipeline_id='<your-pipeline-id>'
+)
