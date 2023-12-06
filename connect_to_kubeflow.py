f7a33e287e213712866e8d6504197ac66cfeaa3c
shraddhab17 <113461811+shraddhab17@users.noreply.github.com>
2023-12-06 22:08:05 +0530

    Update connect_to_kubeflow.py

--- connect_to_kubeflow.py
+++ connect_to_kubeflow.py
@@ -6,7 +6,8 @@ import uuid
 # Get environment variables
 kubeflow_host = os.environ.get('KUBEFLOW_HOST')
 experiment_id = os.environ.get('KUBEFLOW_EXPERIMENT_ID')
-pipeline_id = os.environ.get('KUBEFLOW_PIPELINE_ID')
+pipeline_id = '5eb5350c-77ad-436a-a138-d07a414efbb2'  # Replace with your pipeline ID
+version_id = 'deploy-kube-phish-6-12'  # Replace with your version ID
 pipeline_params = os.environ.get('KUBEFLOW_PIPELINE_PARAMS')
 
 # Parse pipeline parameters as JSON
@@ -20,5 +21,6 @@ client.run_pipeline(
     experiment_id=experiment_id,
     job_name=f'pipeline-run-{uuid.uuid4()}',  # Use a unique identifier as the job name
     pipeline_id=pipeline_id,
+    version_id=version_id,
     params=pipeline_params_json
 )
