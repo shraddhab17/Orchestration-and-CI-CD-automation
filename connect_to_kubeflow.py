7ae2d8fbf4cb991ba6bd1b458f241f470a75048b
shraddhab17 <113461811+shraddhab17@users.noreply.github.com>
2023-12-06 21:44:25 +0530

    Updated connect_to_kubeflow.py

--- connect_to_kubeflow.py
+++ connect_to_kubeflow.py
@@ -1,15 +1,24 @@
 from kfp import Client
+import os
+import json
+import uuid
 
-# Define the host URL of your Kubeflow Pipelines deployment
-host_url = 'http://<your-kubeflow-pipelines-host-url>'
+# Get environment variables
+kubeflow_host = os.environ.get('KUBEFLOW_HOST')
+experiment_id = os.environ.get('KUBEFLOW_EXPERIMENT_ID')
+pipeline_id = os.environ.get('KUBEFLOW_PIPELINE_ID')
+pipeline_params = os.environ.get('KUBEFLOW_PIPELINE_PARAMS')
+
+# Parse pipeline parameters as JSON
+pipeline_params_json = json.loads(pipeline_params)
 
 # Create a client that connects to the Kubeflow Pipelines API
-client = Client(host=host_url)
+client = Client(host=kubeflow_host)
 
-# Run your pipeline with the updated ECR image
+# Run your pipeline with the specified parameters
 client.run_pipeline(
-    experiment_id='<your-experiment-id>', 
-    job_name='<your-job-name>', 
-    pipeline_id='<your-pipeline-id>',
-    params={'image': 'your-account-id.dkr.ecr.your-region.amazonaws.com/your-repo-name:your-tag'}  # replace with your ECR image
+    experiment_id=experiment_id,
+    job_name=f'pipeline-run-{uuid.uuid4()}',  # Use a unique identifier as the job name
+    pipeline_id=pipeline_id,
+    params=pipeline_params_json
 )
