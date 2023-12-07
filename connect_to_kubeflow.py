5091ec05243d1036446efa0ea2af2d7a062999cb
shraddhab17 <113461811+shraddhab17@users.noreply.github.com>
2023-12-07 22:58:48 +0530

    Update connect_to_kubeflow.py

--- connect_to_kubeflow.py
+++ connect_to_kubeflow.py
@@ -48,8 +48,11 @@ params = {'image': '871927043079.dkr.ecr.us-east-1.amazonaws.com/kubeflow'}
 # Hardcoded cookie value
 authservice_session = 'authservice_session=MTcwMTk2NDkyNXxOd3dBTkVWRFZWRkRTVmxZVGt4TFZrVlZURFpMTTBOWFNVTTJXVkJXTlV4VVdUSkZTRGN6UmxCWlJGUlNXRFpVVWtOUk5GRlVWa0U9fCtfhX73aWl3vWpxqQZ4DX-SvWS31ypeW_xCYA20NT9D'
 
+# Append the desired API version to the endpoint URL
+endpoint_with_version = f'{ENDPOINT}/v1beta1'
+
 # Connect to Kubeflow Pipelines Manager
-client = kfp.Client(host=ENDPOINT, cookies=authservice_session, version='v1beta1')
+client = kfp.Client(host=endpoint_with_version, cookies=authservice_session)
 
 # Run pipeline
 client.run_pipeline(
