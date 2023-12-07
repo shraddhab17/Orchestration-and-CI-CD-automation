88e5a8333de6c13c00e2fe54a0926d963cb1063e
shraddhab17 <113461811+shraddhab17@users.noreply.github.com>
2023-12-07 22:36:14 +0530

    Update connect_to_kubeflow.py

--- connect_to_kubeflow.py
+++ connect_to_kubeflow.py
@@ -24,51 +24,12 @@
 #     version_id=version_id,
 #     params=pipeline_params_json
 # )
-
 import kfp
 from datetime import datetime
-import re
 import os
-import mechanize
-from bs4 import BeautifulSoup
-import urllib
-import http.cookiejar as cookielib 
 
 # Get today's date for tags
 today = str(datetime.now())
-
-# Def
-def get_id(text):
-    """
-    Function that retrieves a pipeline's ID from its logs
-    Parameters
-    ----------
-    text : str
-        string version of the logs.
-    Returns
-    -------
-    str : Id of the pipeline.
-    """
-    match = re.search('{\'id\': \'(.+?)\',\\n', text)        
-    if match:
-        found = match.group(1)
-        return(found)
-    
-def get_cookie(text):
-    """
-    Function that retrieves login cookie
-    Parameters
-    ----------
-    text : str
-        string version of the logs.
-    Returns
-    -------
-    str : cookie value.
-    """
-    match = re.search('authservice_session=(.+?) ', text)        
-    if match:
-        found = match.group(1)
-        return(found)
 
 # Parameters
 URL = os.getenv('URL')
@@ -84,17 +45,8 @@ pipeline_id = '5eb5350c-77ad-436a-a138-d07a414efbb2'
 version_id = 'deploy-kube-phish-6-12'
 params = {'image': '871927043079.dkr.ecr.us-east-1.amazonaws.com/kubeflow'}
 
-# Get cookie value
-cj = cookielib.CookieJar()
-br = mechanize.Browser()
-br.set_cookiejar(cj)
-br.open(URL)
-
-br.select_form(nr=0)
-br.form['login'] = EMAIL
-br.form['password'] = PASSWORD
-br.submit()
-authservice_session = 'authservice_session={}'.format(get_cookie(str(cj)))
+# Hardcoded cookie value
+authservice_session = 'authservice_session=MTcwMTk2NDkyNXxOd3dBTkVWRFZWRkRTVmxZVGt4TFZrVlZURFpMTTBOWFNVTTJXVkJXTlV4VVdUSkZTRGN6UmxCWlJGUlNXRFpVVWtOUk5GRlVWa0U9fCtfhX73aWl3vWpxqQZ4DX-SvWS31ypeW_xCYA20NT9D'
 
 # Connect to Kubeflow Pipelines Manager
 client = kfp.Client(host=ENDPOINT, cookies=authservice_session)
